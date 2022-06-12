d631dce82a3611f3d5490f14d45ef28ab2e8f1cf
yayat9744 <yayat9744@gmail.com>
2022-06-12 18:25:35 +0700

    add-challenge-07-yayat

--- promise.js
+++ promise.js
@@ -1,7 +1,13 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let dataTheaterIXX = await promiseTheaterIXX();
+  let dataTheaterVGC = await promiseTheaterVGC();
+  let data = dataTheaterIXX.concat(dataTheaterVGC);
+  let jmlEmosi = data.filter((dataFilm) => dataFilm.hasil === emosi);
+  return jmlEmosi.length;
+};
 
 module.exports = {
   promiseOutput,
